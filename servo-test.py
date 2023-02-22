6bcc99723783d132148c1e6c73c785a835cb9d1f
Hunter Ceranic <45051475+cer-hunter@users.noreply.github.com>
2023-02-22 18:34:31 -0500

    Add files via upload

---
+++ servo-test.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+class servo:
+    def __init__(self, pin):
+        self.pin = pin
+
+    def SetGPIO(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.OUT)
+
+    def SetAngle(self, angle): #could write different positions into this function or could set the positions as defined properties of the class
+        pwm=GPIO.PWM(self.pin, 50)
+        pwm.start(0)
+        duty = angle / 18 + 2
+        GPIO.output(self.pin, True)
+        pwm.ChangeDutyCycle(duty)
+        sleep(1)
+        GPIO.output(self.pin, False)
+        pwm.ChangeDutyCycle(0)
+        pwm.stop()
+
+
+bot_serv = servo(03)
+top_serv = servo(04)
+
+bot_serv.SetGPIO()
+bot_serv.SetAngle(90) #just need to figure out the angles
+top_serv.SetGPIO()
+top_serv.SetAngle(90)
+
+
+GPIO.cleanup()
