843c9661b5ee7b52e2ad4061aedc16893ba6eb99
brlnoble <51976754+brlnoble@users.noreply.github.com>
2023-02-23 17:40:57 -0500

    Working on navbar styling
    Navbar links now highlight when you are on that page. Having trouble updating the images to do the same.

--- app/src/Components/Navbar.js
+++ app/src/Components/Navbar.js
@@ -1,12 +1,26 @@
 import React from "react"
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import styles from "../CSS/Navbar.module.css" //Style sheet for the navigation bar
 
 //Import images
 import logo from "../Images/Logov2-01.svg"
-import user_icon from "../Images/Icons/account_icon.svg"
+import user_icon_unactive from "../Images/Icons/account_icon_unselect.svg"
+import user_icon_active from "../Images/Icons/account_icon_select.svg"
 import menu_icon from "../Images/Icons/menu_icon.svg"
+
+//Setup active style for navlinks
+const active_link = {
+    textDecoration: "none",
+    color: "#f15a24",
+    fontWeight: "bold"
+}
+
+const unactive_link = {
+    textDecoration: "none",
+    color: "inherit",
+}
+
 
 class Navbar extends React.Component {
     render() {
@@ -15,24 +29,22 @@ class Navbar extends React.Component {
 
                 {/*The logo and title sit here*/}
                 <div>
-                    <Link to="/" className={styles.logo_and_title}>
+                    <NavLink to="/" className={styles.logo_and_title}>
                         <img src={logo} alt="logo" className={styles.logo}></img>
                         <h1 className={styles.title}>R.I.P.T.</h1>
-                    </Link>
+                    </NavLink>
                 </div>
             
-                {/*Links to other pages*/}
+                {/*Links to other pages. The styling is all weird cause of the new API*/}
                 <ul className={styles.link}>
-                    <li><Link to="/about" className={styles.react_link}>About us</Link></li>
-                    <li><Link to="/technical" className={styles.react_link}>How it Works</Link></li>
-                    <li><Link to="/sessions" className={styles.react_link}>Sessions</Link></li>
-                    <li><Link to="/trends" className={styles.react_link}>Trends</Link></li>
+                    <li><NavLink to="/about" style={({ isActive }) => isActive ? active_link : unactive_link}>About</NavLink></li>
+                    <li><NavLink to="/technical" style={({ isActive }) => isActive ? active_link : unactive_link}>How it Works</NavLink></li>
+                    <li><NavLink to="/sessions" style={({ isActive }) => isActive ? active_link : unactive_link}>Sessions</NavLink></li>
+                    <li><NavLink to="/trends" style={({ isActive }) => isActive ? active_link : unactive_link}>Trends</NavLink></li>
                 </ul>
 
                 {/*User icon and burger menu*/}
-                <Link to="/profile" className={styles.react_link}>
-                    <img src={user_icon} alt="user_icon" className={styles.icon}></img>
-                </Link>
+                <NavLink to="/profile"><img src={user_icon_unactive} alt="user_icon" className={styles.icon}></img></NavLink> {/*TODO: Update icon on click*/}
                 <img src={menu_icon} alt="menu_icon" className={styles.icon}></img>
             </div>
         )
--- app/src/index.js
+++ app/src/index.js
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="/profile" element={<Profile />} />
 
           <Route path="*" element={<NoPage />} />
         </Route>
