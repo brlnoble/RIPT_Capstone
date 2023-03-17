4b79f248bd221ed4ae2dbb9eb8a8075d8f7ffed6
Hunter Ceranic <45051475+cer-hunter@users.noreply.github.com>
2023-03-17 18:05:56 -0400

    added extra functions for right and left

--- servo-test.py
+++ servo-test.py
@@ -4,33 +4,43 @@ from time import sleep
 bot_serv = AngularServo(12, min_angle = 0, max_angle = 270, min_pulse_width = 0.0005, max_pulse_width = 0.0025)
 top_serv = AngularServo(13, min_angle = 0, max_angle = 270, min_pulse_width = 0.0005, max_pulse_width = 0.0025)
 
-#from arduin0
-# for top servo 
-# jab = 90 
-# uppercut & hook = 10
-# for bottom servo 
-# jab & uppercut = 125 
-# hook = 30
-
-def jab(bot_serv, top_serv):
+def jab_right(bot_serv, top_serv):
     bot_serv.angle = 180
     top_serv.angle = 270
-    sleep(2)
+    sleep(1)
     return 0
 
-def uppercut(bot_serv, top_serv):
+def uppercut_right(bot_serv, top_serv):
     bot_serv.angle = 180
     top_serv.angle = 180
-    sleep(2)
+    sleep(1)
     return 0
 
-def hook(bot_serv, top_serv):
+def hook_right(bot_serv, top_serv):
     bot_serv.angle = 50
     top_serv.angle = 180
-    sleep(2)
+    sleep(1)
+    return 0
+
+def jab_left(bot_serv, top_serv):
+    bot_serv.angle = 180
+    top_serv.angle = 270
+    sleep(1)
+    return 0
+
+def uppercut_left(bot_serv, top_serv):
+    bot_serv.angle = 180
+    top_serv.angle = 180
+    sleep(1)
+    return 0
+
+def hook_left(bot_serv, top_serv):
+    bot_serv.angle = 50
+    top_serv.angle = 180
+    sleep(1)
     return 0
 
 while 1:
-    jab(bot_serv, top_serv)
-    uppercut(bot_serv, top_serv)
-    hook(bot_serv, top_serv)
+    jab_right(bot_serv, top_serv)
+    uppercut_right(bot_serv, top_serv)
+    hook_right(bot_serv, top_serv)
